fe37195bd279c9e702154f04aba5e4549b3568c7
Ira <mazuririnochkacat@gmail.com>
2023-04-10 17:06:47 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppDZ_5/Task3.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDZ_5
+{
+    class BookList
+    {
+        private List<string> books = new List<string>();
+
+        public void Add(string book)
+        {
+            books.Add(book);
+        }
+
+        public void Remove(string book)
+        {
+            books.Remove(book);
+        }
+
+        public bool Contains(string book)
+        {
+            return books.Contains(book);
+        }
+
+        public int Count
+        {
+            get { return books.Count; }
+        }
+
+        public string this[int index]
+        {
+            get { return books[index]; }
+            set { books[index] = value; }
+        }
+
+        public static BookList operator +(BookList bookList, string book)
+        {
+            bookList.Add(book);
+            return bookList;
+        }
+
+        public static BookList operator -(BookList bookList, string book)
+        {
+            bookList.Remove(book);
+            return bookList;
+        }
+    }
+}
